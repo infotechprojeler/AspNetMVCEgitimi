31c976d0ef8235f3ea460172ee059e1a67d43e37
InfotechProjeler <ilica-18@hotmail.com>
2025-02-09 12:57:19 +0300

    9-2-2025 mvc

---
+++ AspNetMVCEgitimi.NetCoreMVC/Controllers/MVC03DataTransferController.cs
@@ -0,0 +1,28 @@
+﻿using AspNetMVCEgitimi.NetCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMVCEgitimi.NetCoreMVC.Controllers
+{
+    public class MVC03DataTransferController : Controller
+    {
+        public IActionResult Index(string txtAra)
+        {
+            // 3 Farklı Yöntemle Controllerdan View a Basit Veriler Gönderebiliriz
+
+            // 1-ViewBag : Tek kullanımlık ömrü vardır.
+            ViewBag.UrunKategorisi = "Bilgisayar"; // Burada ViewBag ismi standart olarak yazılır sonrasında . deyip dilediğimiz değişken adını yazabiliriz.
+            // 2-ViewData : Tek kullanımlık ömrü vardır.
+            var urunListesi = new List<Urun>
+            {
+                new Urun() { Adi = "Oyun Bilgisayarı", Fiyati = 49000, Stok = 5 },
+                new Urun() { Adi = "Laptop", Fiyati = 29000, Stok = 7 },
+                new Urun() { Adi = "İş İstasyonu", Fiyati = 99000, Stok = 3 }
+            };
+            ViewData["Urunler"] = urunListesi;
+            // 3-TempData : 2 kullanımlık ömrü vardır.
+            TempData["UrunBilgi"] = "Toplam " + urunListesi.Count + " Ürün Bulundu..";
+            ViewBag.GetVerisi = txtAra; // query stringle yakaladığımız veriyi viewbag ile ekrana geri yolluyoruz.
+            return View();
+        }
+    }
+}
