eeae54b2b660ed3cd4cd8ed124771724247fdd9e
InfotechProjeler <ilica-18@hotmail.com>
2025-02-09 14:01:24 +0300

    9-2-2025 Mvc

--- NetFrameworkMVC/Controllers/MVC03DataTransferController.cs
+++ NetFrameworkMVC/Controllers/MVC03DataTransferController.cs
@@ -29,5 +29,43 @@ namespace NetFrameworkMVC.Controllers
             ViewBag.GetVerisi = txtAra; // query stringle yakaladığımız veriyi viewbag ile ekrana geri yolluyoruz.
             return View();
         }
+        
+        [HttpPost] // HttpPost attribute ü aşağıdaki metodun post isteğinde çalışacağını ifade eder
+        public ActionResult Index(string txtUrunAdi, string ddlKategori, bool cbOnay, string rbOnay, FormCollection formCollection)
+        {
+            var urunListesi = new List<Urun>
+            {
+                new Urun() { Adi = "Oyun Bilgisayarı", Fiyati = 49000, Stok = 5 },
+                new Urun() { Adi = "Laptop", Fiyati = 29000, Stok = 7 },
+                new Urun() { Adi = "İş İstasyonu", Fiyati = 99000, Stok = 3 }
+            };
+            ViewData["Urunler"] = urunListesi;
+
+            // 1. Yöntem
+            ViewBag.Baslik1 = "1. Yöntem Parametreyle Veri Yakalama";
+            ViewBag.Mesaj1 = "Textbox değeri : " + txtUrunAdi;
+            ViewBag.Mesaj2 = "Dropdown değeri : " + ddlKategori;
+            ViewBag.Mesaj3 = "cbOnay değeri : " + cbOnay;
+            ViewBag.Mesaj3 += " - rbOnay değeri : " + rbOnay;
+
+            // 2. Yöntem
+            ViewBag.Baslik2 = "2. Yöntem FormCollection İle Yakalama";
+            ViewBag.Mesaj4 = "Textbox değeri : " + formCollection["txtUrunAdi"];
+            ViewBag.Mesaj5 = "Dropdown değeri : " + formCollection["ddlKategori"];
+            ViewBag.Mesaj6 = "cbOnay değeri : " + formCollection.GetValues("cbOnay")[0];
+            ViewBag.Mesaj6 += " - rbOnay değeri : " + formCollection.GetValues("rbOnay")[0];
+
+            // 3. Yöntem
+            ViewBag.Baslik3 = "3. Yöntem Request Form İle Yakalama";
+            ViewBag.Mesaj7 = "Textbox değeri : " + Request.Form["txtUrunAdi"];
+            ViewBag.Mesaj8 = "Dropdown değeri : " + Request.Form["ddlKategori"];
+            ViewBag.Mesaj9 = "cbOnay değeri : " + Request.Form.GetValues("cbOnay")[0];
+            ViewBag.Mesaj9 += " - rbOnay değeri : " + Request.Form.GetValues("rbOnay")[0];
+            ViewBag.Mesaj9 += " -- <hr> txtUrunAdi değeri : " + Request.Form.GetValues("txtUrunAdi")[0];
+            ViewBag.Mesaj9 += " -- ddlKategori değeri : " + Request.Form.GetValues("ddlKategori")[0];
+
+
+            return View();
+        }
     }
 }
