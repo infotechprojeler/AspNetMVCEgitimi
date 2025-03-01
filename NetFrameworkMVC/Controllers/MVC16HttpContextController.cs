753cdc21a492749c23f67657e2687950ac7ab774
InfotechProjeler <ilica-18@hotmail.com>
2025-03-01 10:41:31 +0300

    Ege edition

---
+++ NetFrameworkMVC/Controllers/MVC16HttpContextController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace NetFrameworkMVC.Controllers
+{
+    public class MVC16HttpContextController : Controller
+    {
+        // GET: MVC16HttpContext
+        public ActionResult Index()
+        {
+            var mesaj = "RouteData controller : " + RouteData.Values["controller"];
+            mesaj += "<hr/>RouteData Action : " + RouteData.Values["action"];
+            mesaj += "<hr/>RouteData Id : " + RouteData.Values["id"];
+            mesaj += "<hr/>QueryString Kelime : " + HttpContext.Request.QueryString["kelime"];
+            TempData["mesaj"] = mesaj;
+            return View();
+        }
+    }
+}
