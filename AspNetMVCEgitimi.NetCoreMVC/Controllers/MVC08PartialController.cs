ef0e07ac321e5c8007a03e13a54843d089452851
InfotechProjeler <ilica-18@hotmail.com>
2025-03-02 10:12:13 +0300

    2-3-25 v1

---
+++ AspNetMVCEgitimi.NetCoreMVC/Controllers/MVC08PartialController.cs
@@ -0,0 +1,15 @@
+﻿using AspNetMVCEgitimi.NetCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMVCEgitimi.NetCoreMVC.Controllers
+{
+    public class MVC08PartialController : Controller
+    {
+        UyeContext context = new UyeContext();
+        // GET: MVC08Partial
+        public ActionResult Index()
+        {
+            return View(context.Uyeler.ToList());
+        }
+    }
+}
